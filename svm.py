2c336a881697244b2bce9574ed2c0b2e0963d1a5
Yashaswini Sampath <44898518+YashaswiniSampath@users.noreply.github.com>
2024-01-05 16:37:59 -0500

    Update svm.py

--- svm.py
+++ svm.py
@@ -1,10 +1,5 @@
 # Natural Language Toolkit: SVM-based classifier
-#
-# Copyright (C) 2001-2019 NLTK Project
-# Author: Leon Derczynski <leon@dcs.shef.ac.uk>
-#
-# URL: <http://nltk.org/>
-# For license information, see LICENSE.TXT
+
 """
 nltk.classify.svm was deprecated. For classification based
 on support vector machines SVMs use nltk.classify.scikitlearn
